f547586de976fdec94339dcd6e2b69879b5de245
Sebastian Sastre <sebastian@flowingconcept.com>
2016-01-20 20:55:55 -0200

    fixes package

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'flowingconcept:cfs-transcoded-s3',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: 'Storage adapter that uses AWS Elastic Transcoder to transcode and store files in S3'
+  summary: 'Storage adapter that uses AWS Elastic Transcoder to transcode and store files in S3',
   // URL to the Git repository containing the source code for this package.
   git: 'git@github.com:sebastianconcept/Meteor-cfs-transcoded-s3.git',
   // By default, Meteor will default to using README.md for documentation.
